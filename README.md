09b08c7f727d24539b0875227ad47273d78bc191
Emad Aghajani <emadpres@gmail.com>
2023-03-02 23:23:24 +0100

    Improving output format

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Running
 1. `python3 -m venv .venv`
-2. `source .venv/bin/activate`
-3. `pip3 install -r ./requirements`
-4. `flask run --host=0.0.0.0 --port=5500`
+1. `source .venv/bin/activate`
+1. `pip3 install -r ./requirements`
+1. Decice between HTML or Text mode, by setting `HTML_MODE` variable
+1. `flask run --host=0.0.0.0 --port=5500`
--- app.py
+++ app.py
@@ -2,10 +2,9 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+HTML_MODE:bool = False
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def debug(path):  # put application's code here
+def html(path):
     html = f"<h1>URL = <code style='color:blue'>'{request.base_url}'</code></h1>"
     html += f"<h2>Path: '<code style='color:blue'>{path}</code>' = '<code style='color:blue'>{request.full_path}</code>'</h2>"
     html += f"<hr>"
@@ -15,3 +14,21 @@ def debug(path):  # put application's code here
         html += f"<tr><td>{key}</td> <td><code>{value}</code></td></tr>"
     html += "</table>"
     return html
+
+def text(path):
+    text: str = ""
+    text += f"+----------------------------------------------------+\n"
+    text += f"URL  = '{request.base_url}'\n"
+    text += f"Path = '{path}'\n"
+    for key, value in request.headers.items():
+        text += f"    {key: <20} = {value}\n"
+    text += f"+----------------------------------------------------+\n"
+    return text
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def debug(path):  # put application's code here
+    if HTML_MODE:
+        return html(path=path)
+    else:
+        return text(path=path)
